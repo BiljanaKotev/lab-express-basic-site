c87b37b37ed93ce27b4d5c6f70be1f3060a04137
Biljana Kotevska <biljanakotev@gmail.com>
2023-07-01 13:57:18 +0200

    finished lab

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/homepage", (req, res, next) => {
+  console.log(req);
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// app.get("/image", (req, res) => {
+//   res.sendFile(__dirname + "/public/images/Aristotle.jpg");
+// });
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+app.listen(3000, () => console.log("My first app listening on port 3000! "));
